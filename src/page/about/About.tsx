3f558df8e3e927cc91488fec738b70b881efe870
Tanya Bondarenko <taneechkaaa@mail.ru>
2024-10-09 12:11:12 +0700

    Раздел "О ШКОЛЕ, О СЕРФИНГЕ, О КАНАРАХ"

--- src/page/about/About.tsx
+++ src/page/about/About.tsx
@@ -1,9 +1,11 @@
+import { AboutMain } from "./AboutMain";
 import { Header } from "./Header";
 
 export const About = () => {
   return (
     <div>
       <Header />
+      <AboutMain />
     </div>
   );
 };
