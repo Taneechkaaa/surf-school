495e88b6a8dc1f5fdd1541eaf91a99ff2bb9dd5f
Tanya Bondarenko <taneechkaaa@mail.ru>
2024-10-14 11:47:07 +0700

    В страницу ServicesAndCost добавлен раздел "ПАКЕТЫ УСЛУГ"

---
+++ src/page/serviсesAndCost/ServicePackages.tsx
@@ -0,0 +1,53 @@
+export const ServicePackages = () => {
+  return (
+    <div>
+      <h1 className="description-title servicePackages-title">ПАКЕТЫ УСЛУГ</h1>
+      <div className="servicePackages">
+        <div className="packages">
+          <h2 className="servicePackages-subheader">Стандарт</h2>
+          <div className="packages-container">
+            <p className="packages-item">Проживание на вилле 6 ночей</p>
+            <p className="packages-item">Номер категории стандарт</p>
+            <p className="packages-item">Экипировка для обучения</p>
+            <p className="packages-item">
+              Обучение серфингу 5 тренировок по 2 часа
+            </p>
+            <p className="packages-item">Страховка</p>
+            <p className="packages-item">Вечеринка с угощениями и дискотекой</p>
+          </div>
+          <h2 className="servicePackages-subheader">450$</h2>
+        </div>
+
+        <div className="packages">
+          <h2 className="servicePackages-subheader">Стандарт</h2>
+          <div className="packages-container">
+            <p className="packages-item">Проживание на вилле 6 ночей</p>
+            <p className="packages-item">Номер категории стандарт</p>
+            <p className="packages-item">Экипировка для обучения</p>
+            <p className="packages-item">
+              Обучение серфингу 5 тренировок по 2 часа
+            </p>
+            <p className="packages-item">Страховка</p>
+            <p className="packages-item">Вечеринка с угощениями и дискотекой</p>
+          </div>
+          <h2 className="servicePackages-subheader">450$</h2>
+        </div>
+
+        <div className="packages">
+          <h2 className="servicePackages-subheader">Стандарт</h2>
+          <div className="packages-container">
+            <p className="packages-item">Проживание на вилле 6 ночей</p>
+            <p className="packages-item">Номер категории стандарт</p>
+            <p className="packages-item">Экипировка для обучения</p>
+            <p className="packages-item">
+              Обучение серфингу 5 тренировок по 2 часа
+            </p>
+            <p className="packages-item">Страховка</p>
+            <p className="packages-item">Вечеринка с угощениями и дискотекой</p>
+          </div>
+          <h2 className="servicePackages-subheader">450$</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/page/serviсesAndCost/ServicesAndCost.tsx
+++ src/page/serviсesAndCost/ServicesAndCost.tsx
@@ -1,10 +1,13 @@
 import { DescriptionYouWillReceive } from "./DescriptionYouWillReceive";
+import { ServicePackages } from "./ServicePackages";
 import "./servicesAndCost.css";
+import "./servicePackages.css";
 
 export const ServicesAndCost = () => {
   return (
     <div className="servicesAndCost">
       <DescriptionYouWillReceive />
+      <ServicePackages />
     </div>
   );
 };
