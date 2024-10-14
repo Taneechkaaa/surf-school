8a959c0e5b2c3c182aede00cb86619f75d957154
Tanya Bondarenko <taneechkaaa@mail.ru>
2024-10-14 12:22:11 +0700

    В страницу Team добавлен раздел "НАША КОМАНДА"

--- src/page/team/Team.tsx
+++ src/page/team/Team.tsx
@@ -1,7 +1,28 @@
+import "./team.css";
+
 export const Team = () => {
   return (
     <div>
-      <h1>Team</h1>
+      <h1 className="description-title team-title">НАША КОМАНДА</h1>
+      <div className="team-container">
+        <div className="team-card">
+          <img src="/mike.png" alt="" />
+          <p className="teamCard-name">Mike</p>
+          <p className="teamCard-jobTitle">Главный инстуктор</p>
+        </div>
+
+        <div className="team-card">
+          <img className="teamCard-img" src="/Mary.png" alt="" />
+          <p className="teamCard-name">Mary</p>
+          <p className="teamCard-jobTitle">Инстуктор</p>
+        </div>
+
+        <div className="team-card">
+          <img className="teamCard-img" src="/Olga.png" alt="" />
+          <p className="teamCard-name">Olga</p>
+          <p className="teamCard-jobTitle">Инстуктор</p>
+        </div>
+      </div>
     </div>
   );
 };
